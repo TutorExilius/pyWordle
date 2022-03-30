1654a6f5f9b44c6c051e448a0278e231518ef247
Tutor Exilius <tutorexilius@gmail.com>
2022-03-30 22:55:09 +0200

    feat: use asyncqt + bring asyncio and qt together

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import sys
+import asyncio
 
 from PySide2.QtWidgets import QApplication
+from asyncqt import QEventLoop
 
 from pywordle.view.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
 
     window = MainWindow()
     window.show()
 
-    sys.exit(app.exec_())
+    with loop:
+        sys.exit(loop.run_forever())
 
 
 if __name__ == '__main__':
