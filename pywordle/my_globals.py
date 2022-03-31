04a0b3957bd3795695ae3f52e325091a0e8979a6
Tutor Exilius <tutorexilius@gmail.com>
2022-03-31 07:51:33 +0200

    feat: renamed database + set app version + added pytest packaged

--- pywordle/my_globals.py
+++ pywordle/my_globals.py
@@ -4,8 +4,6 @@ App globals
 
 from pathlib import Path
 
-from pywordle.logic.helper import get_app_major_version
-
 WORKING_DIR = Path(__file__).absolute().parent.parent
-DATABASE_FILE = f"db_v{get_app_major_version(WORKING_DIR)}.X.sqlite3"
+DATABASE_FILE = "db.sqlite3"
 DATABASE_URL = f"sqlite:///{WORKING_DIR}/{DATABASE_FILE}"
